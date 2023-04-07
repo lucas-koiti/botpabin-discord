28b5fc3533c6e3d6c39e0d1d9afd092e8dd0aac6
lkoiti <lucaskoitig@gmail.com>
2023-04-07 02:10:35 -0300

    fix client connection

--- botpabin.js
+++ botpabin.js
@@ -1,21 +1,30 @@
 import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const auth = require('./auth.json')
 import { randomChoice } from './botauxfuncs.js'
 
+const require = createRequire(import.meta.url)
+const auth = require('./auth.json')
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+let bot_secret_token = auth.token
+
+client.login(bot_secret_token)
+
 client.on('ready', () => {
+
     console.log("Connected as " + client.user.tag)
 })
 
-client.on('message', (receivedMessage) => {
-    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+client.on('messageCreate', msg => {
+
+    if (msg.author == client.user) { // Prevent bot from responding to its own messages
+
         return
     }
     
-    if (receivedMessage.content.startsWith("!")) {
-        processCommand(receivedMessage)
+    if (msg.content.startsWith("!")) {
+
+        processCommand(msg)
     }
 })
 
@@ -25,19 +34,24 @@ function processCommand(receivedMessage) {
     let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
     let args = splitCommand.slice(1) // All other words are args/parameters/options for the command
 
-
     if (primaryCommand == "help") {
+
         callHelp(args, receivedMessage)
     } else if (primaryCommand == "random") {
+
         callRandom(args, receivedMessage)
     } else {
+
         receivedMessage.channel.send("loading ...")
     }
 }
 
 function callHelp(args, receivedMessage){
+
     receivedMessage.channel.send("Te vira bixo, it's not rocket science")
+
     setTimeout(function() {
+
                 receivedMessage.channel.send("just kidding, try use !random [comp/top/mid/sup/jg/adc]")
             }, 2000)
 }
@@ -46,18 +60,17 @@ function callRandom(args, receivedMessage){
     let result = ""
 
     if (args.length == 0){
+
         receivedMessage.channel.send("Dude, c'mon, specifies what u want - comp/top/mid/sup/jg/adc")
     } 
     else if (args.legnt > 1) {
+
         receivedMessage.channel.send("Dude, c'mon, choose one - comp/top/mid/sup/jg/adc")
     } else {
+
         result = randomChoice(args)
         receivedMessage.channel.send(result)
     }
 }
 
-
-let bot_secret_token = auth.token
-
-client.login(bot_secret_token)
 
